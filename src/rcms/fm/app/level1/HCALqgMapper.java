d8fae8f1ac34c418a908b44fb1a3ddf4687a11d4
John Hakala <john_hakala@brown.edu>
2018-04-10 12:39:01 -0400

    add helper methods to qgMapper, add test logs

--- src/rcms/fm/app/level1/HCALqgMapper.java
+++ src/rcms/fm/app/level1/HCALqgMapper.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import rcms.fm.app.level1.HCALqgMapper.level1qgMapper;
 import rcms.fm.fw.parameter.type.MapT;
+import rcms.fm.fw.parameter.type.ParameterType;
 import rcms.fm.fw.parameter.type.StringT;
 import rcms.fm.fw.parameter.type.VectorT;
 import rcms.fm.fw.user.UserActionException;
@@ -23,6 +24,28 @@ import rcms.util.logger.RCMSLogger;
 /**
  * @author John Hakala
  * class for mapping out the qualified group of a particular configuration
+ * 
+ * the most important thing here is the level1qgMapper
+ * if you call level1qgMapper it should return a full representation of the qualified group (or at least only what we care about)
+ * Example (this is made up, for clarity):
+ * {
+ *   HCAL_HBHE: {
+ *                 Executive_0: {N/A:[hcalSupervisor_0, hcalPartitionViewer_0]},
+ *                 Executive_1: {41 :[hcal::DTCManager_0, hcalCrate_0]},
+ *              },
+ *   HCAL_HF  : {
+ *                  Executive_2: {N/A: [hcalPartitionViewer_1, hcalSupervisor_1]}
+ *                  Executive_3: {42 : [hcalTrivialFU_0, hcalEventBuilder_0, hcal::DTCReadout_0, DummyTriggerAdapter_0, hcal::uHTRManager_1, hcal::DTCManager_2, hcalCrate_1]},
+ *              }
+ *  }
+ *  
+ *  the whole map is called the "l1qgMap"
+ *  an individual key-value pair in the l1qgMap is called the "l2map"
+ *  the value in that key-value pair is called the "execMap"
+ *  the execMap should have only one key-value pair
+ *  the key of the exec map is the crate number it corresponds to, or "N/A" if not applicable
+ *  the key-value pair of the execMap is called the "crateMap"
+ *  the value of the crateMap is a list of applications and is called the "appList"
  */
 public class HCALqgMapper {
 
@@ -89,6 +112,7 @@ public class HCALqgMapper {
 
   /**
    * class for mapping out a level1 FM's QG 
+   * it makes the l1QGmap
    */
   public class level1qgMapper extends abstractQGmapper {
     protected Resource functionManagerResource = null;
@@ -101,21 +125,99 @@ public class HCALqgMapper {
      */
     public level1qgMapper(Resource l1FMqr, QualifiedGroup qg) throws UserActionException {
       this.qg = qg;
-      MapT<MapT<MapT<VectorT<StringT>>>> l2Map = new MapT<MapT<MapT<VectorT<StringT>>>>();
+      MapT<MapT<MapT<VectorT<StringT>>>> l1QGmap = new MapT<MapT<MapT<VectorT<StringT>>>>();
       List<QualifiedResource> l2FMlist = qg.seekQualifiedResourcesOfType(new FunctionManager());
       for (QualifiedResource qr: l2FMlist) {
         try {
           Group l2group = qg.rs.retrieveLightGroup(qr.getResource());
           List<Resource> level2execs = l2group.getChildrenResources();
           level2qgMapper level2mapper = new level2qgMapper(level2execs);
-          MapT<MapT<VectorT<StringT>>> level2map = (MapT<MapT<VectorT<StringT>>>) level2mapper.getMap();
-          l2Map.put(qr.getName(), level2map);
+          MapT<MapT<VectorT<StringT>>> execMap = (MapT<MapT<VectorT<StringT>>>) level2mapper.getMap();
+          l1QGmap.put(qr.getName(), execMap);
         }
         catch (UserActionException | DBConnectorException e) {
           throw new UserActionException(e.getMessage());
         }
       }
-      qgMap = l2Map;
+      qgMap = l1QGmap;
+      if (!isQGmapValid()) {
+        throw new UserActionException("Error creating the QG map!");
+      }
+    }
+    
+    /**
+     * get the executive corresponding to a given crate
+     * @param crateNumber, the crate number
+     * @return a string of the executive's name, e.g. Executive_5
+     * @throws UserActionException if the crate couldn't be found
+     */
+    public String getExecOfCrate(Integer crateNumber) throws UserActionException{
+      for (ParameterType<?> l2FMmap : qgMap.getMap().values()) {
+        MapT<MapT<VectorT<StringT>>> l2Map = ((MapT<MapT<VectorT<StringT>>>) l2FMmap);
+        for (MapT<VectorT<StringT>> execMap : l2Map.getMap().values()) {
+          if (execMap.getMap().keySet().contains(new StringT(Integer.toString(crateNumber)))){
+            return execMap.getMap().keySet().toArray()[0].toString(); // execs should only be associated with one crate
+          }
+        }
+      }
+      throw new UserActionException("Could not find executive corresponding to crate " + crateNumber);
+    }
+
+    /**
+     * return the crate corresponding to an application, specifying which FM the app belongs to
+     * @param appName the application's name
+     * @param fmName the FM's name
+     * @return a string of the crate number
+     */
+    public String getCrateOfApp(String appName, StringT fmName) {
+      String crate = "N/A";
+      MapT<MapT<VectorT<StringT>>> execMap = (MapT<MapT<VectorT<StringT>>>) qgMap.getMap().get(fmName);
+      for (StringT crateKey : execMap.getMap().keySet()) {
+        String thisCrate = crateKey.toString();
+        MapT<VectorT<StringT>> crateMap = execMap.get(crateKey);
+        for (VectorT<StringT> appList : crateMap.getMap().values()) {
+          if (appList.contains(new StringT(appName))) {
+            crate =thisCrate; 
+          }
+        }
+      }
+      return crate;
+    }
+
+    /**
+     * return the crate corresponding to an application
+     * @param appName the app's name
+     * @return a string of the crate number
+     */
+    public String getCrateOfApp(String appName) {
+      String crate = "N/A";
+      for (StringT fmName : qgMap.getMap().keySet()) {
+         crate = getCrateOfApp(appName, fmName);
+         if (!crate.equals("N/A")) break;
+      }
+      return crate;
+    }
+
+    /**
+     * sanity check that the l1qgMap is valid
+     * the rules for validity are:
+     * a) there is exactly one crate corresponding to one executive
+     * b) there is exactly one appList per executive
+     * @return a bool where 1 is valid and 0 is not valid
+     */
+    private boolean isQGmapValid () {
+      for (StringT level2key : qgMap.getMap().keySet()) {
+        MapT<MapT<VectorT<StringT>>> execMap = (MapT<MapT<VectorT<StringT>>>) qgMap.getMap().get(level2key);
+        if (execMap.getMap().keySet().size() > 1) {
+          return false;
+        }
+        for (MapT<VectorT<StringT>> crateMap : execMap.getMap().values()) {
+          if (crateMap.getMap().keySet().size() > 1) {
+            return false;
+          }
+        }
+      }
+      return true;
     }
   }
 }
